13b0f317a7fcf4417e960ea821f947dd09fa7e1b
Spencer Linkous <spencerlinkous@icloud.com>
2021-10-12 15:51:17 -0400

    did stuff

--- RPS2.py
+++ RPS2.py
@@ -1,15 +1,16 @@
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import random
+# import random
 
 # Where the player chooses
-
-PlayerChoice = input("Rock, Paper, or Scissors?")
+# playerIn = input("Rock, Paper, or Scissors?")
 # friendlyness
-if PlayerChoice == "rock" or PlayerChoice == "Rock" or PlayerChoice == "r" or PlayerChoice == "R":
-    PlayerChoice = "Rock"
-elif PlayerChoice == "paper" or PlayerChoice == "Paper" or PlayerChoice == "p" or PlayerChoice == "P":
-    PlayerChoice = "Paper"
-elif PlayerChoice == "scissors" or PlayerChoice == "Scissors" or PlayerChoice == "s" or PlayerChoice == "S":
-    PlayerChoice = "Scissors"
-if PlayerChoice != "Rock" and PlayerChoice != "Paper" and PlayerChoice != "Scissors":
+
+# import time
+# def functionthing(number):
+#     for owo in range(number):
+#         print(owo + 1)
+#         # time.sleep(1)
+#
+# howmany = int(input("howmany"))
+#
+# functionthing(number=howmany)
