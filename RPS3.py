73337ab57d6945fbf2c5c190f40e5a4a703ec549
Spencer Linkous <slinkousart@gmail.com>
2021-11-15 16:23:25 -0500

    Started writing things, broke things...

--- RPS3.py
+++ RPS3.py
@@ -1,7 +1,20 @@
-# import required stuff
-from time import sleep
-import os
 import random
 
-print("test")
 
+class RPSItem:
+    def __init__(self, wins):
+        self.wins = wins
+
+
+Rock = RPSItem(["Scissors"])
+Paper = RPSItem(["Rock"])
+Scissors = RPSItem(["Paper"])
+
+playerChoice = input('rock paper or scissors')
+compChoice = random.choice(["Rock", "Paper", "Scissors"])
+
+if playerChoice is compChoice:
+    result = 'Tie'
+elif compChoice in
+
+print('You chose', playerChoice, "\nThe computer chose", compChoice)
