b6628d1b41ce89a805a6797bbcb2ed2e1b08b2ae
Spencer Linkous <slinkousart@gmail.com>
2021-12-02 18:09:59 -0500

    now detects if it needs to create stats.json or add a new user

--- RPS4.py
+++ RPS4.py
@@ -2,26 +2,37 @@ import json
 from os import path
 
 
+def write(toWrite):
+    with open('stats.json', 'w', encoding='utf-8') as f:
+        json.dump(toWrite, f, ensure_ascii=False, indent=4)
+
+
+username = input('input your username\n')
 if path.exists('stats.json') is False:
     x = {
-        "slinkous": {
+        username: {
             "wins": 0,
             "ties": 0,
             "losses": 0
         }
     }
-    with open('stats.json', "w", encoding='utf-8') as f:
-        json.dump(x, f, ensure_ascii=False, indent=4)
+    write(toWrite=x)
 
 
-username = input('input your username\n')
 with open('stats.json', 'r', encoding='utf-8') as infile:
     globalStats = json.load(infile)
+
+if username not in globalStats:
+    globalStats[username] = {
+        "wins": 0,
+        "ties": 0,
+        "losses": 0
+    }
+    write(toWrite=globalStats)
 
 userStats = globalStats[username]
 stat = input('choose what to change')
 new = int(input('input the stat to change'))
 userStats[stat] = new
 
-with open('stats.json', 'w', encoding='utf-8') as f:
-    json.dump(globalStats, f, ensure_ascii=False, indent=4)
+write(toWrite=globalStats)
